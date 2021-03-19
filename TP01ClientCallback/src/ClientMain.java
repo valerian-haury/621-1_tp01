f17874883e79c712d1cefcfe3094e71edd3b08ff
Valérian Haury <haury.valerian@gmail.com>
2021-03-19 12:59:06 +0100

    add client httpclient call

--- TP01ClientCallback/src/ClientMain.java
+++ TP01ClientCallback/src/ClientMain.java
@@ -1,15 +1,55 @@
+import java.net.Authenticator;
+import java.net.InetSocketAddress;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpClient.Redirect;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
-/* import javax.ws.rs.core.MediaType;
-import org.apache.http.*;
-import org.apache.http.client.*;
-import org.apache.http.impl.client.*;
-import org.apache.http.client.methods.*;
-*/
 public class ClientMain {
 
-	public static void main(String[] args) throws Exception { 
-		String url = "http://localhost:8080/TP01ServiceCallback/service/calcul";
-		//A completer
+	public static void main(String[] args) throws Exception {
 
-   }
+		String url = "http://localhost:8080/TP01ServiceCallback/service/calcule";
+		String responseServiceUrl = "http://localhost:8080/TP01ServiceCallback/serviceCallback/resultat";
+
+		/*
+		 * HttpClient client = HttpClientBuilder.create().build(); HttpPost request =
+		 * new HttpPost(url); request.setHeader("Content-Type", "text/plain");
+		 * request.setHeader("Location", responseServiceUrl); try { HttpResponse
+		 * response = client.execute(request);
+		 * System.out.println(response.getStatusLine().getStatusCode()); } catch
+		 * (IOException e) { System.err.println(e); }
+		 */
+		HttpClient client = HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1)
+				.connectTimeout(Duration.ofSeconds(5)).build();
+
+		HttpRequest request = HttpRequest.newBuilder().POST(BodyPublishers.ofString(url)).uri(URI.create(url))
+				.setHeader("Content-Type", "text/plain").setHeader("Location", responseServiceUrl).build();
+
+		CompletableFuture<HttpResponse<String>> asyncResponse = null;
+		asyncResponse = client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+
+		int resultStatusCode = 0;
+
+		try {
+			resultStatusCode = asyncResponse.thenApply(HttpResponse::statusCode).get(5, TimeUnit.SECONDS);
+
+		} catch (InterruptedException | ExecutionException | TimeoutException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Status Code: " + resultStatusCode);
+
+	}
 }
